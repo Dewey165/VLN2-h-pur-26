c70ffe16ed64e9abec3cd78d080d178df3f3002a
Dewey165 <agi2211@gmail.com>
2016-04-29 14:07:28 +0000

    vs2015 models

---
+++ Mooshak26solution/Mooshak26/Models/Entities/Milestone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak26.Models.Entities
+{
+    /// <summary>
+    /// Each assignments will have one or more milestones to represents each part of the assigment to hand in.
+    /// For example, one milestone can be "Part 1" of the assignment and another milestone can be "Part 2"
+    /// </summary>
+    public class Milestone
+    {
+
+    }
+}
---
+++ Mooshak26solution/Mooshak26/Services/AssignmentsService.cs
@@ -0,0 +1,17 @@
+﻿using Mooshak26.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak26.Services
+{
+    public class AssignmentsService
+    {
+        public List<AssignmentViewModel> GetAssignmentsInCourse(int courseID)
+        {
+            //TODO: 
+            return null;
+        }
+    }
+}
